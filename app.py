7d51dd0655b45676fd3f335d4592e74fac6a4581
Dan Carmel <dan.carmel.mail@gmail.com>
2019-08-09 13:56:37 +0300

    Added files

---
+++ app.py
@@ -0,0 +1,38 @@
+
+import os
+from notion.client import NotionClient
+from flask import Flask
+from flask import request
+
+
+app = Flask(__name__)
+if app.config["ENV"] == "production":
+    app.config.from_object("config.ProductionConfig")
+else:
+    app.config.from_object("config.DevelopmentConfig")
+
+print(f'ENV is set to: {app.config["ENV"]}')
+
+
+def createNotionTask(token, collectionURL, content):
+    # notion
+    client = NotionClient(token)
+    cv = client.get_collection_view(collectionURL)
+    row = cv.collection.add_row()
+    row.title = content
+
+
+# @app.route('/')
+# def hello_world():
+#     createNotionTask(token_v2, url, "flask executes via request")
+#     return 'got it'
+
+
+@app.route('/create_todo', methods=['GET'])
+def create_todo():
+
+    todo = request.args.get('todo')
+    token_v2 = app.config["TOKEN"]
+    url = app.config["URL"]
+    createNotionTask(token_v2, url, todo)
+    return f'added {todo} to Notion'
