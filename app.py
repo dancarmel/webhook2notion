54511c37f704021f80139179cb0253698bb830ba
Dan Carmel <dan.carmel.mail@gmail.com>
2019-08-09 14:14:11 +0300

    added files

--- app.py
+++ app.py
@@ -37,3 +37,9 @@ def create_todo():
     url = app.config["URL"]
     createNotionTask(token_v2, url, todo)
     return f'added {todo} to Notion'
+
+
+if __name__ == '__main__':
+    app.debug = True
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
