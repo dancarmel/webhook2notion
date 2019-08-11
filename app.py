cd4b30ea82cf27d56373edb0901d9ad363765bd0
Dan Carmel <dan.carmel.mail@gmail.com>
2019-08-11 16:51:40 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from flask import request
 app = Flask(__name__)
 
 
-def createNotionTask(token, collectionURL, content, author):
+def createTweet(token, collectionURL, content, author):
     # notion
     client = NotionClient(token)
     cv = client.get_collection_view(collectionURL)
@@ -17,14 +17,73 @@ def createNotionTask(token, collectionURL, content, author):
     row.profile = author
 
 
-@app.route('/tweet', methods=['GET'])
-def tweet():
+def createTask(token, collectionURL, content):
+    # notion
+    client = NotionClient(token)
+    cv = client.get_collection_view(collectionURL)
+    row = cv.collection.add_row()
+    row.name = content
+
+
+def createReceipt(token, collectionURL, product, content, date):
+    # notion
+    client = NotionClient(token)
+    cv = client.get_collection_view(collectionURL)
+    row = cv.collection.add_row()
+    row.product = product
+    row.content = content
+    row.date = date
+
+
+def createEmail(token, collectionURL, sender, subject, messageURL):
+    # notion
+    client = NotionClient(token)
+    cv = client.get_collection_view(collectionURL)
+    row = cv.collection.add_row()
+    row.sender = sender
+    row.subject = subject
+    row.messageURL = messageURL
+
+
+@app.route('/twitter', methods=['GET'])
+def twitter():
     tweet = request.args.get('tweet')
     author = request.args.get('author')
     token_v2 = os.environ.get("TOKEN")
     url = os.environ.get("URL")
-    createNotionTask(token_v2, url, tweet, author)
+    createTweet(token_v2, url, tweet, author)
     return f'added {tweet} to Notion'
+
+
+@app.route('/todoist', methods=['GET'])
+def todoist():
+    todo = request.args.get('todo')
+    token_v2 = os.environ.get("TOKEN")
+    url = os.environ.get("URL")
+    createTask(token_v2, url, todo)
+    return f'added {todo} to Notion'
+
+
+@app.route('/gmailreceipts', methods=['GET'])
+def gmailReceipt():
+    product = request.args.get('product')
+    content = request.args.get('content')
+    date = request.args.get('date')
+    token_v2 = os.environ.get("TOKEN")
+    url = os.environ.get("URL")
+    createReceipt(token_v2, url, product, content, date)
+    return f'added {product} receipt to Notion'
+
+
+@app.route('/createemail', methods=['GET'])
+def gmailUrgentEmail():
+    sender = request.args.get('sender')
+    subject = request.args.get('subject')
+    messageURL = request.args.get('messageURL')
+    token_v2 = os.environ.get("TOKEN")
+    url = os.environ.get("URL")
+    createReceipt(token_v2, url, sender, subject, messageURL)
+    return f'added email from {sender} to Notion'
 
 
 if __name__ == '__main__':
